d448653723244b9f9a2181a9871de776252fb6a9
Sujan Lam <sujann42@gmail.com>
2024-06-28 19:40:56 -0500

    more details

--- src/test/java/com/kafka/KafkaSpringbootApplicationTests.java
+++ src/test/java/com/kafka/KafkaSpringbootApplicationTests.java
@@ -3,32 +3,27 @@ package com.kafka;
 import com.kafka.dto.Customer;
 import com.kafka.services.KafkaMessagePublisher;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-class KafkaSpringbootApplicationTests {
-
-	static DockerImageName myImage = DockerImageName.parse("confluentinc/cp-kafka:latest").asCompatibleSubstituteFor("apache/kafka");
+public class KafkaSpringbootApplicationTests {
 
 	@Container
-    static KafkaContainer kafka = new KafkaContainer(myImage);
-
+	static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
 
 	@DynamicPropertySource
 	public static void initKafkaProperties(DynamicPropertyRegistry registry) {
@@ -40,10 +35,11 @@ class KafkaSpringbootApplicationTests {
 
 	@Test
 	public void testSendEventsToTopic() {
-		publisher.sendEventsToTopic(new Customer(263, "test_user", "test@gmail.com", "3232323"));
-		await().pollInterval(Duration.ofSeconds(3)).atMost(10, SECONDS).untilAsserted(() -> {
-			//asserts
-		});
+		publisher.sendEventsToTopic(new Customer(263, "test user", "test@gmail.com", "564782542752"));
+		await().pollInterval(Duration.ofSeconds(3))
+				.atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+					// assert statement
+				});
 	}
 
 }
